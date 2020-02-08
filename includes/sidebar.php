ce6b75781f6f8571948f1a6cd4cf47d60dffefe7
JIAXING CHEN <justinchen044@gmail.com>
2020-02-08 17:00:13 -0500

    lol

---
+++ includes/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidenav">
+    <a href="#">Home</a>
+    <a href="#">Financial Well Being</a>
+    <a href="#">Stop Juuling Lmao</a>
+    <a href="#">Helth</a>
+    <a href="#">Stop Your Crippling Crack Addiction</a>
+</div>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset = 'UTF-8'/>
+        <title>Dont forgot to insert title here</title>
+        <link rel = 'stylesheet' href = 'styles/main.css'/>
+    </head>
+    
+    <body>
+        <aside> <?php include("includes/sidebar.php"); ?> </aside>
+        
+        <?php include("includes/header.php"); ?>
+
+        <main>
+            <div class = "container">
+                <div>
+                    <h2>Today's Goal</h2>
+                    <ul>
+                        <li>Hang ye self</li>
+                        <li>Insert Here</li>
+                        <li>Insert more shit</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>This month's new goal</h2>
+                    <ul>
+                        <li>Eat arse</li>
+                        <li>GET THE NODE WORKING HO</li>
+                        <li>Har har diddly dar ye are a pirate</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>This Year</h2>
+                    <ul>
+                        <li>What do I do lord</li>
+                        <li>Destroy the Child</li>
+                        <li>Corrupt them all</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
